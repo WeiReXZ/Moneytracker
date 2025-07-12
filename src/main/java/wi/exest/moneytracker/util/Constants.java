051a54559bcceaa7f3e564c5568c75664b85030d
WeiReXZ <wellzforward@gmail.com>
2025-07-12 15:09:51 +0300

    Delete src directory

--- src/main/java/wi/exest/moneytracker/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package wi.exest.moneytracker.util;
-
-
-public class Constants {
-    public static final String TYPE_INCOME = "income";
-    public static final String TYPE_EXPENSE = "expense";
-}
